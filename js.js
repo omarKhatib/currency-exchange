e4bfad467f78fc44647d86f261239b730a157826
omarkhatib <omarkh1995@yahoo.com>
2017-02-02 17:19:43 +0200

    without design II

--- js.js
+++ js.js
@@ -1,31 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
     get();
 });
 
+function get() {
+    $.ajax({
+        url: "http://api.fixer.io/latest?base=USD"
+        , type: 'GET'
+        , success: function (result) {
+            for (var item in result.rates) {
+                $('#to').append('<option value =' + result.rates[item] + '>' + item + '</option>');
+            }
+        }
+    });
+}
 
-function get(){
-   $.ajax({
-    url: "http://api.fixer.io/latest?base=USD",
-    type: 'GET',
-    success: function(result) {
-        
-         for (var item in result.rates){ $('#list').append('<option value ='+result.rates[item]+'>'+item+'</option>');
-                                   }
-     
-    }
-  }); 
-
-    }
-
-
-
-function exchange(){
-    
-   var input= parseFloat($('#in').val());
-    var ex = parseFloat($('#list').val());
+function exchange() {
+    var input = parseFloat($('#in').val());
+    var ex = parseFloat($('#to').val());
     $('#res').val((input * ex));
-    
-    
-    
-    
 }
